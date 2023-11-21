fa40a742facab2a52e67296533fa4d0aa5405624
Sunkaiah <150812123+Sunkaiah@users.noreply.github.com>
2023-11-20 23:36:02 -0800

    Add files via upload

---
+++ append ass 20N/ass 20 N.py
@@ -0,0 +1,42 @@
+#append():
+squares=[]
+for x in range(5):
+    square=x**2
+    squares.append(square)
+print(squares)
+ 
+list=["A","B","C","D"]
+list.append("E")
+print(list)
+
+l=[]
+for i in range(21):
+    if i%2==0:
+        l.append(i)
+print(l)
+ 
+stack=[]
+stack.append('apple')
+stack.append('banana')
+stack.append('mango')
+print(stack)
+stack.pop()
+
+n=10
+l=[]
+for item in range(n):
+    list.append()
+print(n)
+
+list=[]
+i=0
+while i < 2:
+    j=0
+    while j < 2:
+        if j == 1:
+            break
+        list.append([i,j])
+        j+=1
+    i+=1
+
+
---
+++ forloop ass 20N/Untitled-1.py
@@ -0,0 +1,73 @@
+n=input("enter the number")
+count=0
+for i in n:
+    print("charecter present in",count,"index",i)
+    count+=2
+    count+=3
+    
+#forloop statement (for each element and do action)    
+n=input("enter the word")
+count=0
+for i in n:
+    print("charecter the present in",count,"index",i)
+    count+=2
+    count+=4
+    
+
+n=input("enter the word:")
+count=0
+for i in n:
+    print("charecter present in ",count,"index is ",i)
+    count+=2
+    count+=3
+    
+    
+n=input("enter the word : ")
+count=0
+for i in n:
+    count+=1
+    print("charecter present in",count,"index is",i) 
+
+
+#forloop statement
+n=input("enter the word : ")
+count=0
+for i in n:
+    count+=1
+    print("charecter the present in",count,"index is",i)
+    count+=2
+    count+=4 
+
+n=input("enter the word : ")
+count=0
+for i in n:
+    print("charecter the present in",count,"index is",i)
+    print("sunku")
+    if 15<30:
+        print("come to")
+    else:
+        print("dance")
+        
+        
+n=input("enter the name")
+for i in n:
+    count+=1
+    count+=2
+    count+=3
+    print("charecter the present in",count,"index is",i)
+
+
+s="please follow me"
+for i in string:
+    print(i)
+
+count=0
+list=["sunku","pandu","chinna"]
+while len(list)>0:
+    list.pop()
+    count+=1
+    print(count)
+print(count)
+
+
+
