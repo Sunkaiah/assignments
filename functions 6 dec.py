9748aa4ec4b6ecb3eebc24636db6b9ed53f5a264
Sunkaiah <150812123+Sunkaiah@users.noreply.github.com>
2023-12-20 20:32:09 -0800

    Add files via upload

---
+++ functions 6 dec.py
@@ -0,0 +1,24 @@
+# DELETED ():
+x=[5,10,15,20,25]
+del x[2]
+print(x)
+
+#
+a=15
+b=30
+c=a+b+15+30-5
+print(c)
+del c
+
+#
+item1=[1,2,3,4,5,6,7]
+item2=["a","b","c","d","e"]
+print(item1)
+print(item2)
+del item1[2]
+print(item1)
+
+
+
+
+
