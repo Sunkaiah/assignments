06b2d88d5103b4412634b5649ac03774dc1b497d
Sunkaiah <150812123+Sunkaiah@users.noreply.github.com>
2023-12-28 21:41:29 -0800

    Add files via upload

---
+++ relation operators/relation operators.py
@@ -0,0 +1,71 @@
+
+#relational operators
+a=2
+b=8
+print(a>b) answer :False
+print(a<b) answer :True
+print(a>=b) answer :False
+print(a<=b) answer :True
+
+a=10.5
+b=8.5
+print(a>b) answer :True
+print(a<b) answer :False
+print(a>=b) answer :True
+print(a<=b) answer :False
+
+a=True
+b=False
+print(a>b) answer :True
+print(a<b) answer :False
+print(a>=b) answer :True
+print(a<=b) answer :False
+
+a="sunku"
+b="pandu"
+print(a>b) answer :False
+print(a<b) answer :True
+print(a>=b) answer :False
+print(a<=b) answer :True
+
+a=10
+b=5.5
+print(a>b) answer :True
+print(a<b) answer :False
+print(a>=b) answer :True
+print(a<=b) answer :False
+
+a=10
+b=False
+print(a>b) answer :True
+print(a<b) answer :False
+print(a>=b) answer :True
+print(a<=b) answer :False
+      
+a=4
+b="hari"
+#print(a>b)TypeError: '>' not supported between instances of 'int' and 'str'
+#print(a<b)TypeError: '<' not supported between instances of 'int' and 'str'
+#print(a>=b)TypeError: '>=' not supported between instances of 'int' and 'str'
+#print(a<=b)TypeError: '<=' not supported between instances of 'int' and 'str'
+
+a=15.5
+b=False
+print(a>b) answer :True
+print(a<b) answer :False
+print(a>=b) answer :True
+print(a<=b) answer :False
+
+a=10.5
+b="sudharshan"
+#print(a>b)TypeError: '>' not supported between instances of 'float' and 'str'
+#print(a<b)TypeError: '<' not supported between instances of 'float' and 'str'
+#print(a>=b)TypeError: '>=' not supported between instances of 'float' and 'str'
+#print(a<=b)TypeError: '<=' not supported between instances of 'float' and 'str'
+
+a=True
+b="nagesh"
+#print(a>b)TypeError: '>' not supported between instances of 'bool' and 'str'
+#print(a<b)TypeError: '<' not supported between instances of 'bool' and 'str'
+#print(a>=b)TypeError: '>=' not supported between instances of 'bool' and 'str'
+#print(a<=b)TypeError: '<=' not supported between instances of 'bool' and 'str'
